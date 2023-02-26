37dadc14525332297fed9d8ead072244ca274cbd
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-02-26 17:33:57 +0000

    Initial commit

---
+++ src/AppMentors.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+export default function AppMentor() {
+  const [person, setPerson] = useState({
+    name: '엘리',
+    title: '개발자',
+    mentor: [{
+      name: '밥',
+      title: '시니어개발자',
+      },
+      {
+      name: '제임스',
+      title: '시니어개발자',
+      },
+   ],
+  });
+  return (
+    <div>
+      <h1>
+        {person.name}는 {person.title}
+      </h1>
+      <p> {person.name}의 멘토는 : </p>
+      <ul>
+        {person.mentors.map((mentor, index) => {
+          <li key={index}>
+            {mentor.name} ({mentor.title})
+          </li>
+        })}
+      </ul>
+      <button
+        onClick={() => {
+          const prev = promt(`누구의 이름을 바꾸고 싶은가요?`);
+          const current = promt(`이름을 무엇으로 바꾸고 싶은가요?`);
+        }}
+      >
+        멘토의 이름 바꾸기
+      </button>
+    </div>
+  );
+}
