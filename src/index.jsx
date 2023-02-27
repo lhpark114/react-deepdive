14bc79e66df49e34d066c83bac7d8657087c2173
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-02-27 00:56:14 +0000

    AppWrap - Navbar

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppForm from './AppForm';
+import AppWrap from './AppWrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppForm />
+    <AppWrap />
   </React.StrictMode>
 );
