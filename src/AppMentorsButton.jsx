0b78258cdae2391178b2eb0569de94bd6c0f9496
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-02-28 04:21:27 +0000

    AppMentorsButton - useMemo, useCallback, memo

--- src/AppMentorsButton.jsx
+++ src/AppMentorsButton.jsx
@@ -1,25 +1,25 @@
-import React, { useReducer } from 'react';
+import React, { memo, useCallback, useMemo, useReducer } from 'react';
 import personReducer from './reducer/person-reducer';
-export default function AppMentor() {
-  // const [person, setPerson] = useState(initialPerson);
+
+export default function AppMentorsButton() {
   const [person, dispatch] = useReducer(personReducer, initialPerson);
   
-  const handleUpdate = () => {
+  const handleUpdate = useCallback(() => {
     const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
     const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
     dispatch({type:'updated', prev, current});  
-  };
+  }, []);
 
-  const handleAdd = () => {
+  const handleAdd = useCallback(() => {
     const name = prompt(`멘토의 이름은?`);
     const title = prompt(`멘토의 직함은?`);
     dispatch({type:'added', name, title});  
-    };
+    }, []);
   
-  const handleDelete = () => {
+  const handleDelete = useCallback(() => {
     const name = prompt(`누구를 삭제하고 싶은가요?`);
     dispatch({type:'deleted', name });  
-    }
+    }, []);
     
   return (
     <div>
@@ -41,23 +41,30 @@ export default function AppMentor() {
   );
 }
 
-function Button({ text, onClick }) {
-  console.log('Button', text, 're-rendering 😜');
-  return (
-    <button
-      onClick={onClick}
-      style={{
-        backgroundColor: 'black',
-        color: 'white',
-        borderRadius: '20px',
-        margin: '0.4rem',
-      }}
-    >
-      {text}
-    </button>
-  );
-}
+const Button = memo(({ text, onClick }) => {
+    console.log('Button', text, 're-rendering 😜');
+    const result = useMemo(() => calculateSomething(), []);
+    return (
+      <button
+        onClick={onClick}
+        style={{
+          backgroundColor: 'black',
+          color: 'white',
+          borderRadius: '20px',
+          margin: '0.4rem',
+        }}
+      >
+        {`${text} ${result}`}
+      </button>
+    );
+  });
 
+function calculateSomething() {
+  for (let i = 0; i < 10000; i++) {
+    console.log('😆');
+  }
+  return 10;
+}
 
 const initialPerson = {
     name: '엘리',
